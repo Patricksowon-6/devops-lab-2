be94c127b25ac2914f042f25948c29c0a48a3088
Patricksowon-6 <patrickakins03@gmail.com>
2025-02-12 12:49:19 +0000

    Average Code

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int num1 = 4;
+    int num2 = 10;
+    int num3 = 7;
+
+    int average = (num1 + num2 + num3) / 3;
+
+    printf("Average is: %d", average);
+}
